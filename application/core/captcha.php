1cb87d4703be3306fb73518c7c431add7d7c31be
Isaev Timur <timka_issaev@mail.ru>
2020-12-25 08:20:10 +0300

    was improved script. added the set functions, fixed bugs

--- application/core/captcha.php
+++ application/core/captcha.php
@@ -1,12 +1,23 @@
 <?php
 class Captcha {
-  public $img_width=110;
-  public $img_height=43;
-  public $img_font_size=14;
+  public $width=110;
+  public $height=43;
+  public $font_size=14;
   public $let_amount = 5;
   public $fon_let_amount=5;
   public $letters = array(1,2,3,4,5,6,7,8,9);
   public $colors = array(90,110,130,150);
+   
+  public $font = null;//Путь к шрифту
+
+  public function __construct($font = '') {
+    if(!empty($font)) {
+      $this->font = $font;
+    } else {
+      $this->font = SITE_ROOT.'/fonts/arial.ttf';
+    }
+  }
+
   /**
    * @desc Проверка кода введенного пользоватлем со значем в СЕССИИ
    * @param <Int> code - код введённый пользователем
@@ -15,17 +26,37 @@ class Captcha {
     return ($code == $_SESSION['captcha_code']) ? true : false;
   }
 
-  public function generate() {    
-    $width = $this->img_width; 
-    $height = $this->img_height;
-    $font_size = $this->img_font_size;
-    $let_amount = $this->let_amount;//Количество главных символоа
-    $fon_let_amount = $this->fon_let_amount;// Символы на фоне
-    $font = SITE_ROOT.'/fonts/arial.ttf';//Путь к шрифту
-    $letters = $this->letters;
-    $colors = $this->colors;
- 
-    $src = imagecreatetruecolor($width,$height);//создаем изображение               
+  public function setWidth($width) {
+    $this->width = $width;
+  }
+
+  public function setHeight($height) {
+    $this->height = $height;
+  }
+
+  public function setFontSize($font_size) {
+    $this->font_size = $font_size;
+  }
+
+  public function setLetAmount($let_amount) {
+    $this->let_amount = $let_amount;
+  }
+
+  public function setFonLetAmount($fon_let_amount) {
+    $this->fon_let_amount = $fon_let_amount;
+  }
+
+  public function setLetters($letters) {
+    $this->this->letters = $letters;  
+  }
+
+  public function setColors($colors) {
+    $this->this->colors = $colors;
+  }
+
+  public function generate() {
+
+    $src = imagecreatetruecolor($this->width,$this->height);//создаем изображение               
 
     $fon = imagecolorallocate($src,rand(247, 255),rand(247, 255),rand(247, 255));//создаем фон
     imagefill($src,0,0,$fon);//заливаем изображение фоном
@@ -35,27 +66,27 @@ class Captcha {
       imageline($src,0,rand()%50,200,rand()%50,$line_color);
     }
 
-    for($i=0;$i < $fon_let_amount;$i++){//добавляем на фон буковки
+    for($i=0;$i < $this->fon_let_amount;$i++){//добавляем на фон буковки
       $color = imagecolorallocatealpha($src,rand(0,255),rand(0,255),rand(0,255),100);//случайный цвет
-      $letter = $letters[rand(0,sizeof($letters)-1)];//случайный символ
-      $size = rand($font_size-3 ,$font_size+3);//случайный размер                                                
+      $letter = $this->letters[rand(0,sizeof($this->letters)-1)];//случайный символ
+      $size = rand($this->font_size-3 ,$this->font_size+3);//случайный размер                                                
       imagettftext($src,$size,rand(0,15),
-                   rand($width*1,$width-$width*2),
-                   rand($height*0.2,$height),$color,$font,$letter);
+                   rand($this->width*1,$this->width-$this->width*2),
+                   rand($this->height*0.2,$this->height),$color,$this->font,$letter);
     }
  
-    for($i=0;$i < $let_amount;$i++){//то же самое для основных букв
+    for($i=0;$i < $this->let_amount;$i++){//то же самое для основных букв
       $color = imagecolorallocatealpha($src,
-                                       $colors[rand(0,sizeof($colors)-1)],
-                                       $colors[rand(0,sizeof($colors)-1)],
-                                       $colors[rand(0,sizeof($colors)-1)],
+                                       $this->colors[rand(0,sizeof($this->colors)-1)],
+                                       $this->colors[rand(0,sizeof($this->colors)-1)],
+                                       $this->colors[rand(0,sizeof($this->colors)-1)],
                                        rand(20,40)); 
-      $letter = $letters[rand(0,sizeof($letters)-1)];
-      $size = rand($font_size*2-2,$font_size*2+2);
-      $x = ($i+1.2)*$font_size;//даем каждому символу случайное смещение
-      $y = (($height*2)/2.7) + rand(0,5);                            
+      $letter = $this->letters[rand(0,sizeof($this->letters)-1)];
+      $size = rand($this->font_size*2-2,$this->font_size*2+2);
+      $x = ($i+1.2)*$this->font_size;//даем каждому символу случайное смещение
+      $y = (($this->height*2)/2.7) + rand(0,5);                            
       $cod .= $letter;//запоминаем код
-      imagettftext($src,$size,rand(0,15),$x,$y,$color,$font,$letter);
+      imagettftext($src,$size,rand(0,15),$x,$y,$color,$this->font,$letter);
     }
     header('Content-type: image/png'); 
     $_SESSION['captcha_code'] = $cod;
